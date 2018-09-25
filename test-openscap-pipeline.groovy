5e26c2e3e98a9836e42856effea2992c2495996a
Ivan Udovichenko <iudovichenko@mirantis.com>
2018-09-25 20:47:56 +0300

    Latest Vanya's changes

--- test-openscap-pipeline.groovy
+++ test-openscap-pipeline.groovy
@@ -7,8 +7,9 @@
  *  SALT_MASTER_CREDENTIALS     Credentials to the Salt API.
  *
  *  XCCDF_BENCHMARKS_DIR        The XCCDF benchmarks base directory (default /usr/share/xccdf-benchmarks/mirantis/)
- *  XCCDF_BENCHMARKS            List of XCCDF benchmarks filenames (e.g. manila/openstack_manila-xccdf.xml,horizon/openstack_horizon-xccdf.xml)
- *  XCCDF_PROFILE               The XCCDF profile to evaluate (default 'default')
+ *  XCCDF_BENCHMARKS            List of pairs XCCDF benchmark filename and corresponding profile separated with ','
+ *                                  these pairs are separated with semicolon.
+ *                                  (e.g. manila/openstack_manila-xccdf.xml,profilename;horizon/openstack_horizon-xccdf.xml,profile)
  *  XCCDF_VERSION               The XCCDF version (default 1.2)
  *  XCCDF_TAILORING_ID          The tailoring id (default None)
  *
@@ -23,12 +24,78 @@
  */
 
 
+
+/**
+  * Upload results to the `WORP` dashboard
+  *
+  * @param apiUrl               The base dashboard api url
+  * @param cloudName            The cloud name (mostly, the given node's domain name)
+  * @param nodeName             The node name
+  * @param results              The scanning results
+  */
+def uploadResultToDashboard(apiUrl, cloudName, nodeName, results) {
+    // Yes, we do not care of performance and will create at least 4 requests per each result
+    def requestData = [:]
+
+    def cloudId
+    def nodeId
+
+    // Let's take a look, may be our minion is already presented on the dashboard
+    // Get available environments
+    environments = common.parseJSON(http.sendHttpGetRequest("${apiUrl}/environment/"))
+    for (environment in environments) {
+        if (environment['name'] == cloudName) {
+            cloudId = environment['uuid']
+            break
+        }
+    }
+    // Cloud wasn't presented, let's create it
+    if (! cloudId ) {
+        // Create cloud
+        resuestData['name'] = cloudName
+        cloudId = common.parseJSON(http.sendHttpPostRequest("${apiUrl}/environment/", requestData))['env']['uuid']
+
+        // And the node
+        // It was done here to reduce count of requests to the api.
+        // Because if there was not cloud presented on the dashboard, then the node was not presented as well.
+        requestData['nodes'] = [nodeName]
+        nodeId = common.parseJSON(http.sendHttpPutRequest("${apiUrl}/environment/${cloudId}/nodes/", requestData))['uuid']
+    }
+
+    if (! nodeId ) {
+        // Get available nodes in our environment
+        nodes = common.parseJSON(http.sendHttpGetRequest("${apiUrl}/environment/${cloudId}/nodes/"))
+        for (node in nodes) {
+            if (node['name'] == nodeName) {
+                nodeId = node['id']
+                break
+            }
+        }
+    }
+
+    // Node wasn't presented, let's create it
+    if (! nodeId ) {
+        // Create node
+        requestData['nodes'] = [nodeName]
+        nodeId = common.parseJSON(http.sendHttpPutRequest("${apiUrl}/environment/${cloudId}/nodes/", requestData))['uuid']
+    }
+
+    // Get report_id
+    requestData['env_uuid'] = cloudId
+    def reportId = common.parseJSON(http.sendHttpPostRequest("${apiUrl}/reports/openscap/", requestData))['report']['uuid']
+
+    // Upload results
+    requestData['results'] = results
+    requestData['node_name'] = nodeName
+    http.sendHttpPutRequest("${apiUrl}/reports/openscap/${reportId}/", requestData)
+}
+
+
 node('python') {
     def pepperEnv = 'pepperEnv'
 
     // XCCDF related variables
-    def xccdfProfile = XCCDF_PROFILE ?: 'default'
-    def benchmarksArray = XCCDF_BENCHMARKS.tokenize(',')
+    def benchmarksAndProfilesArray = XCCDF_BENCHMARKS.tokenize(';')
     def benchmarksDir = XCCDF_BENCHMARKS_DIR ?: '/usr/share/xccdf-benchmarks/mirantis/'
     def xccdfVersion = XCCDF_VERSION ?: '1.2'
     def xccdfTailoringId = XCCDF_TAILORING_ID ?: 'None'
@@ -63,75 +130,32 @@ node('python') {
 
         for (minion in liveMinions) {
 
-            // Set necessary variables for uploading the scanning results to the dashboard
-            if (UPLOAD_TO_DASHBOARD.toBoolean()) {
-                if (common.validInputParam('DASHBOARD_API_URL')) {
-                    def cloudName = salt.getGrain(pepperEnv, minion, 'domain')['return'][0].values()[0].values()[0]
-                    def requestData = [:]
-
-                    def cloudId
-                    def nodeId
-
-                    // Let's take a look, may be our minion is already presented on the dashboard
-                    // Get available environments
-                    environments = common.parseJSON(http.sendHttpGetRequest("${DASHBOARD_API_URL}/environment/"))
-                    for (environment in environments) {
-                        if (environment['name'] == cloudName) {
-                            cloudId = environment['uuid']
-                            break
-                        }
-                    }
-                    // Cloud wasn't presented, let's create it
-                    if (! cloudId ) {
-                        // Create cloud
-                        resuestData['name'] = cloudName
-                        cloudId = common.parseJSON(http.sendHttpPostRequest("${DASHBOARD_API_URL}/environment/", requestData))['env']['uuid']
-                    }
-
-                    // Get available nodes in our environment
-                    nodes = common.parseJSON(http.sendHttpGetRequest("${DASHBOARD_API_URL}/environment/${cloudId}/nodes/"))
-                    for (node in nodes) {
-                        if (node['name'] == minion) {
-                            nodeId = node['id']
-                            break
-                        }
-                    }
-                    // Node wasn't presented, let's create it
-                    if (! nodeId ) {
-                        // Create node
-                        requestData['nodes'] = [minion]
-                        nodeId = common.parseJSON(http.sendHttpPutRequest("${DASHBOARD_API_URL}/environment/${cloudId}/nodes/", requestData))['uuid']
-                    }
-
-                } else {
-                    throw new Exception('Uploading to the dashboard is enabled but the DASHBOARD_API_URL was not set')
-                }
-            }
-
             // Iterate oscap evaluation over the benchmarks
-            for (benchmark in benchmarksArray) {
+            for (benchmark in benchmarksAndProfilesArray) {
+                def (benchmarkFilePath, profile) = benchmark.tokenize(',').collect({it.trim()})
+
                 // Remove extension from the benchmark name
-                def benchmarkPathWithoutExtension = benchmark.replaceFirst('[.][^.]+$', '')
+                def benchmarkPathWithoutExtension = benchmarkFilePath.replaceFirst('[.][^.]+$', '')
                 // And build resultsDir based on this path
                 def resultsDir = "${resultsBaseDir}/${benchmarkPathWithoutExtension}"
 
-                def benchmarkFile = "${benchmarksDir}${benchmark}"
+                def benchmarkFile = "${benchmarksDir}${benchmarkFilePath}"
 
                 // Evaluate the benchmark
                 salt.runSaltProcessStep(pepperEnv, minion, 'oscap.eval', [
-                    'xccdf', benchmarkFile, xccdfProfile, xccdfVersion, xccdfTailoringId, resultsDir
+                    'xccdf', benchmarkFile, "results_dir=${resultsDir}",
+                    "profile=${profile}", "xccdf_version=${xccdfVersion}",
+                    "tailoring_id=${xccdfTailoringId}"
                 ])
 
                 // Attempt to upload the scanning results to the dashboard
                 if (UPLOAD_TO_DASHBOARD.toBoolean()) {
-                    // Get report_id
-                    requestData['env_uuid'] = cloudId
-                    def reportId = common.parseJSON(http.sendHttpPostRequest("${DASHBOARD_API_URL}/reports/openscap/", requestData))['report']['uuid']
-
-                    // Upload results
-                    requestData['results'] = salt.GetFileContent(pepperEnv, minion, "${resultsDir}/results.json")
-                    requestData['node_name'] = minion
-                    http.sendHttpPutRequest("${DASHBOARD_API_URL}/reports/openscap/${reportId}/", requestData)
+                    if (common.validInputParam('DASHBOARD_API_URL')) {
+                        def cloudName = salt.getGrain(pepperEnv, minion, 'domain')['return'][0].values()[0].values()[0]
+                        uploadResultToDashboard(DASHBOARD_API_URL, cloudName, minion, salt.getFileContent(pepperEnv, minion, "${resultsDir}/results.json"))
+                    } else {
+                        throw new Exception('Uploading to the dashboard is enabled but the DASHBOARD_API_URL was not set')
+                    }
                 }
             }
         }
@@ -147,11 +171,10 @@ node('python') {
 
                 dir(destDir) {
                     // Archive scanning results on the remote target
-                    salt.runSaltProcessStep(pepperEnv, minion, 'file.remove', tempArchive)
                     salt.runSaltProcessStep(pepperEnv, minion, 'archive.tar', ['czf', tempArchive, resultsBaseDir])
 
                     // Get it content and save it
-                    writeFile file: destination, text: salt.GetFileContent(pepperEnv, minion, tempArchive)
+                    writeFile file: destination, text: salt.getFileContent(pepperEnv, minion, tempArchive)
 
                     // Remove scanning results and the temp archive on the remote target
                     salt.runSaltProcessStep(pepperEnv, minion, 'file.remove', resultsBaseDir)
@@ -179,7 +202,7 @@ node('python') {
 
             // Build Artifactory spec object
             def uploadSpec = """{
-                "files": 
+                "files":
                     [
                         {
                             "pattern": "${zipName}",
